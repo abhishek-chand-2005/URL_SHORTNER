55efd1c5c14cc2ff5206789796bf2e20e6dc67f0
Abhishek Chand <0101abhi0101@gmail.com>
2025-09-25 21:45:45 +0530

    Basics Backend, modularising, and errorHandle

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./src/config/mongo.config.js";
+import shortUrl from './src/routes/short_url.route.js'
+import { redirectFromShortUrl } from "./src/controller/short_url.controller.js";
+import { errorHandler } from "./src/utils/errorHandler.js";
+
+
+
+dotenv.config("./.env")
+const app = express();
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// routes
+app.use('/api/create', shortUrl)
+app.get('/:id', redirectFromShortUrl)
+
+
+app.use(errorHandler)
+
+// listen on port 3000
+app.listen(3000, ()=>{
+    connectDB()
+    console.log("Server is running on port 3000")
+})
